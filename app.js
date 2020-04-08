0cdcf90223824e579da0d026af963c2aa6c7f4e9
miiiiiin <min.songkyung@gmail.com>
2020-04-09 00:38:43 +0900

    set db connection

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require('express')
 const app  = express()
 const mustacheExpress = require('mustache-express')
 const bodyParser = require('body-parser')
+const pgp = require('pg-promise')()
 const PORT = 3000
+const CONNECTION_STRING = "postgres://localhost:5432/newsdb"
 
 //configuring view engine
 app.engine('mustache', mustacheExpress())
@@ -11,6 +13,9 @@ app.set('view engine', 'mustache')
 
 //bodyparser middleware
 app.use(bodyParser.urlencoded({extended: false}))
+
+//create db object
+const db = pgp(CONNECTION_STRING)
 
 
 //create route
@@ -22,10 +27,21 @@ app.post('/register', (req, res) => {
     let username = req.body.username
     let password = req.body.password
 
-    console.log(username)
-    console.log(password)
+    //return you either 1 record or none
+    db.oneOrNone('SELECT userid FROM users WHERE username = $1', [username])
+    .then((user) => {
+        if (user) {//if it's exist
+            res.render('register', {message: "User name already exist"})
+        } else {
+            //insert user into the users table
+            db.none('INSERT INTO users(username, password) VALUES($1, $2)', [username, password])
+            .then(() => {
+                res.send('SUCCESS')
 
-    res.send("REGISTER")
+            })
+        }
+    })
+
 })
 
 app.listen(PORT, () => {
