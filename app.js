70199b62e515f79aef65e987f06124f24f393a2a
miiiiiin <min.songkyung@gmail.com>
2020-04-14 00:43:04 +0900

    retrieve&update

--- app.js
+++ app.js
@@ -34,20 +34,42 @@ app.get('/users/add-article', (req, res) => {
     res.render('add-article')
 })
 
+app.get('/users/articles/edit/:articleId', (req, res) => {
+    let articleId = req.params.articleId
+    db.one('SELECT articleid,title,body FROM articles WHERE articleid = $1', [articleId])
+    .then((article) => {
+        res.render('edit-article', article)
+    })
+})
+
 app.post('/users/add-article', (req, res) => {
     let title = req.body.title
     let description = req.body.description
     let userId = req.session.user.userId
 
-    db.none('INSERT INTO articles(title,body,userid) VALUES($1, $2, $3)',[title, description, userId])
+    db.none('INSERT INTO articles(title,body,userid) VALUES($1,$2,$3)',[title,description,userId])
     .then(() => {
         res.send("SUCCESS")
     })
-    
 })
 
-app.get('/users/articles', (req, res) => {
-    res.render('articles',{username: req.session.user.username})
+app.get('/users/articles', (req, res) => { //route
+    let userId = 5//req.session.user.userId
+    db.any('SELECT articleid,title,body FROM articles WHERE userid = $1', [userId])
+    .then((articles) => {
+        res.render('articles', {articles: articles})
+    })
+})
+
+app.post('/users/update-article', (req, res) => {
+    let title = req.body.title
+    let description = req.body.description
+    let articleId = req.body.articleId
+
+    db.none('UPDATE articles SET title = $1, body = $2 WHERE articleid = $3',[title,description,articleId])
+    .then(() => {
+        res.redirect('/users/articles')
+    })
 })
 
 app.post('/login', (req, res) => {
